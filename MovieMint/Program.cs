9923302b7b3be9d23f4ea6efb4c4303256d0a949
Ruo Huang <huangrobert2015@gmail.com>
2024-03-05 18:29:01 -0500

    Add error handling

--- MovieMint/Program.cs
+++ MovieMint/Program.cs
@@ -16,9 +16,16 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+if (app.Configuration.GetValue<bool>("UseDeveloperExceptionPage"))
+    app.UseDeveloperExceptionPage();
+else
+    app.UseExceptionHandler("/error");
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.MapGet("/error", () => Results.Problem());
 
 app.MapControllers();
 
